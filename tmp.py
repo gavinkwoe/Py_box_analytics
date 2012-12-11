0bd2b150f5feb0b0abe794d8be57a3cd528ddfb7
Yannan Wang <tmotw1015@gmail.com>
2012-12-11 11:11:53 +0800

    Minor changes
    Added new filter of a full list of manufacturers

--- tmp.py
+++ tmp.py
@@ -19,7 +19,7 @@ import MySQLdb
 # # print prefix_map[:100]
 
 # x = '00:16:44:69:ab:7a'
-x = '00:26:bb:0e:93:27'
+# x = '00:26:bb:0e:93:27'
 
 # try:
 # 	a = str(prefix_map.xs(x[0:8].upper()))
@@ -38,21 +38,56 @@ x = '00:26:bb:0e:93:27'
 # 	try: 
 # 		print str(row['phone_id'][0:8].upper())
 # 		manuf = manuf.append(str(row['phone_id'][0:8].upper()))
-# 		print manuf
-# 	except:
-# 		# manuf = manuf.append('Not Phone')
-# 		pass
-# 		print manuf
+# # 		print manuf
+# # 	except:
+# # 		# manuf = manuf.append('Not Phone')
+# # 		pass
+# # 		print manuf
 
-# print manuf
+# # print manuf
 
-prefix_map = pd.read_table('prefix_mapping.txt', delimiter='\t', names=['Prefix','Manuf'], index_col='Prefix')
-	# apply_filter = lambda x: try: prefix_map.xs(x['phone_id'][0:8].upper()) except: 'Not phone'
-	# clean['manuf'] = clean.apply(apply_filter, axis=1)
+# prefix_map = pd.read_table('prefix_mapping.txt', delimiter='\t', names=['Prefix','Manuf'], index_col='Prefix')
+# 	# apply_filter = lambda x: try: prefix_map.xs(x['phone_id'][0:8].upper()) except: 'Not phone'
+# 	# clean['manuf'] = clean.apply(apply_filter, axis=1)
 
-print prefix_map.index
+# print prefix_map.index
 
-if x[0:8].upper() in prefix_map.index:
-	print 'yes'
-else:
-	print 'no'
+# if x[0:8].upper() in prefix_map.index:
+# 	print 'yes'
+# else:
+# 	print 'no'
+
+
+# read raw data from MySQL table: box_upload
+con 	 = MySQLdb.connect("localhost","guest","guest123","TEST" )
+cur 	 = con.cursor()
+read_raw = "SELECT * FROM box_upload LIMIT 0, 200"
+
+cur.execute(read_raw)
+results = cur.fetchall()
+
+raw              = pd.DataFrame(list(results), columns=['SQL_ID','Ping_Date', 'Ping_Time', 'Strength', \
+														'BSSID', 'Destination(DA)', 'Source(SA)', 'Type'])
+parse_datetime   = lambda x: pd.datetools.dateutil.parser.parse(x, yearfirst=True)
+raw['Date_Time'] = raw['Ping_Date'].map(str) + ' ' + raw['Ping_Time']
+raw['Date_Time'] = raw['Date_Time'].apply(parse_datetime)
+raw 			 = raw.drop(['SQL_ID','Ping_Date','Ping_Time','BSSID','Destination(DA)'], axis=1)
+raw 			 = raw.reindex(columns=['Date_Time','Strength','Source(SA)','Type'])
+
+
+preq = raw[raw['Type']=='PREQ'].reset_index(drop=True, inplace=True)
+pres = raw[raw['Type']=='PRES'].reset_index(drop=True, inplace=True)
+
+# print preq[:99]
+# print pres[:99]
+
+count = 0
+
+for row_index, row in preq.iterrows():
+	# print row['Source(SA)']
+	# print '='*20
+	if row['Source(SA)'] in pres['Source(SA)'].values:
+		count += 1
+		print row['Source(SA)']
+
+print count
